614aa682b60367e1c9ebe5b51fe8baa8b982e7c6
Stanislav Aituhanov <saituhanov@teramind.co>
2025-10-26 22:37:03 +0200

    Auth ready

---
+++ src/utils/jwt.ts
@@ -0,0 +1,19 @@
+import { SignJWT } from "jose";
+import { createSecretKey } from "crypto";
+import env from "../../env.ts";
+
+export interface JWTPayload {
+	id: string;
+	email: string;
+	username: string;
+}
+
+export const generateToken = (payload: JWTPayload) => {
+	const secret = env.JWT_SECRET;
+	const secretKey = createSecretKey(secret, "utf-8");
+	return new SignJWT(payload)
+		.setProtectedHeader({ alg: "HS256" })
+		.setIssuedAt()
+		.setExpirationTime(env.JWT_EXPIRES_IN)
+		.sign(secretKey);
+};
